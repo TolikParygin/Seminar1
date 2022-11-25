ca8d0b87c31a2f95ab7f82e1a57dae744e17b0cd
Tolik <anatolijparygin988@gmail.com>
2022-11-25 21:14:35 +0300

    является ли квадратом

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 2 числа ");
+int nomberA = int.Parse(Console.ReadLine());
+int nomberB = int.Parse(Console.ReadLine());
+
+if(nomberA/nomberB==nomberB)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");    
+}
