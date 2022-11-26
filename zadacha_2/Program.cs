656d6b2c7045d586526ff0ce71e6468c3505ebec
Tolik <anatolijparygin988@gmail.com>
2022-11-26 21:37:49 +0300

    вывод чисел от -N до N

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1, 2"
+Console.Clear();
+Console.WriteLine("Введите число:");
+int nomber = int.Parse(Console.ReadLine());
+int count=-nomber;
+if (nomber>=0)
+    while(count<=nomber)
+    {
+        Console.Write(count + ", ");
+        count++;
+    }
+else
+while(count>=nomber)
+{
+Console.Write(nomber +", ");
+nomber++;
+}
+
