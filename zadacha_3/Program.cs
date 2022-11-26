ae5ec37aaf93298c2e9f36a73dcced6dcb48f185
Tolik <anatolijparygin988@gmail.com>
2022-11-26 21:48:13 +0300

    вывод последней цифры трехзначного числа

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine());
+int N3=N%10;
+Console.WriteLine(N3);
